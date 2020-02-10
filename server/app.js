3c598aeb6deb23dbe3b7b9f9e3a92115099e14b9
jetly sandita <jetly27sandita@gmail.com>
2020-02-10 11:58:23 +0700

    delete config

--- server/app.js
+++ server/app.js
@@ -4,7 +4,9 @@ const port = 3000
 const cors = require('cors')
 const routes = require('./routes')
 const errorHandler = require('./helpers/errorHandler')
-
+if(process.env.NODE_ENV === 'development'){
+  require('dotenv').config()
+}
 app.use(cors())
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
